96cbeb942481c1d580c3297b8bb66e47d0a59c95
manuel <manuel.hernandez28@outlook.com>
2025-06-06 22:40:07 -0600

    feat: Agregar funcionalidades para crear y eliminar repuestos, y actualizar las plantillas correspondientes

--- Aplicaciones/Taller/urls.py
+++ Aplicaciones/Taller/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),  # * Maps the root URL to the index view
     path('repuestos/', views.repuestos, name="repuestos"),  # * Mapas de la URL 'repuestos/' a la vista repuestos
+    path('crear_repuesto/', views.crear_repuesto, name="crear_repuesto"),
+    path('eliminar_repuesto/<int:id_repuesto>/', views.eliminar_repuesto, name="eliminar_repuesto"),  # * mapea la URL 'eliminar_repuesto/<id_repuesto>/' a la vista eliminar_repuesto
 ]
 
--- Aplicaciones/Taller/views.py
+++ Aplicaciones/Taller/views.py
@@ -1,6 +1,7 @@
 # ! views.py Aplicaciones/Taller
 from django.shortcuts import render
 from .models import Repuesto
+from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
@@ -17,3 +18,37 @@ def repuestos(request):
      * Render the list of repuestos.
     """
     return render(request, 'repuestos.html', {'repuestos': repuestosdbb})  # * Pasa los repuestos a la plantilla 'repuestos.html' 
+
+def crear_repuesto(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre_repuesto')
+        descripcion = request.POST.get('descripcion')
+        precio = request.POST.get('precio')
+        cantidad_disponible = request.POST.get('cantidad_disponible')
+
+
+        Repuesto.objects.create(
+            nombre_repuesto=nombre,
+            descripcion=descripcion,
+            precio=precio,
+            cantidad_disponible=cantidad_disponible
+        )
+        
+        return redirect('repuestos')
+    else:
+        return render(request, 'crear_repuesto.html')
+    
+def eliminar_repuesto(request, id_repuesto):
+    """
+     * Elimina un repuesto de la base de datos.
+    """
+    try:
+        repuesto = Repuesto.objects.get(id_repuesto=id_repuesto)
+        repuesto.delete()
+        return redirect('repuestos')
+    except Repuesto.DoesNotExist:
+        return render(request, 'error.html', {'message': 'Repuesto no encontrado.'})  # * Manejo de error si el repuesto no existe
+    
+        
+        
+        
