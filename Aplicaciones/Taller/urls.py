0dcf61d6908cddbfaad7beddb8f62aa08b8bf6c8
manuel <manuel.hernandez28@outlook.com>
2025-06-06 20:31:12 -0600

    Refactor code structure for improved readability and maintainability

--- Aplicaciones/Taller/urls.py
+++ Aplicaciones/Taller/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Maps the root URL to the index view
+    path('', views.index, name='index'),  # * Maps the root URL to the index view
+    path('repuestos/', views.repuestos, name="repuestos"),  # * Mapas de la URL 'repuestos/' a la vista repuestos
 ]
+
--- Aplicaciones/Taller/views.py
+++ Aplicaciones/Taller/views.py
@@ -1,5 +1,6 @@
 # ! views.py Aplicaciones/Taller
 from django.shortcuts import render
+from .models import Repuesto
 
 # Create your views here.
 def index(request):
@@ -7,3 +8,12 @@ def index(request):
      * Render the index page of the Taller application.
     """
     return render(request, 'index.html')
+
+def repuestos(request):
+    
+    repuestosdbb = Repuesto.objects.all()  # * Recibe todos los repuestos de la base de datos
+    
+    """
+     * Render the list of repuestos.
+    """
+    return render(request, 'repuestos.html', {'repuestos': repuestosdbb})  # * Pasa los repuestos a la plantilla 'repuestos.html' 
